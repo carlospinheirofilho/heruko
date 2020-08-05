a04feed050745910dc131d253ca22a86b35cc5a9
carlospinheirofilho <carlospinheiro@ic.ufal.br>
2020-08-05 10:30:08 -0300

    Configuring heroku

--- client_manager/settings.py
+++ client_manager/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=8t0xdz^8@czss!pl$xj0q@+be5&otc!yee+9dk_z%8&z9ssd0'
-
+#SECRET_KEY = '=8t0xdz^8@czss!pl$xj0q@+be5&otc!yee+9dk_z%8&z9ssd0'
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['client-manager-carlos.herokuapp.com/']
 
 
 # Application definition
@@ -75,6 +77,16 @@ WSGI_APPLICATION = 'client_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = { 
+    'default': config('DATABASE_URL', 
+    default=default_dburl, 
+    cast=dburl), 
+    }
+
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -85,6 +97,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''
 
 
 # Password validation
@@ -132,3 +145,5 @@ MEDIA_ROOT = 'media'
 LOGIN_URL = '/login/'
 
 LOGIN_REDIRECT_URL = 'person_list'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
